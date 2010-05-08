a56b3be6093300829e80acea13e08579a18f40b1
austyfrosty <austyfrosty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-08 01:54:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/comment-validation-reloaded/trunk@238549 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- comment-validation-reloaded.php
+++ comment-validation-reloaded.php
@@ -219,7 +219,7 @@ function cvr_author() {
 	$author = $comm['author'];
 	
 	if ( $author != false && ( is_singular() && comments_open() ) )
-		echo '<p id="comment-validation-reloaded-author" style="font-size:80%;"><a class="twitter-anywhere-user" href="http://austinpassy.com/wordpress-plugins/comment-validation-reloaded" title="WordPress Comment Validation Reloaded plugin">Comment validation by @<a href="http://twitter.com/thefrosty" title="Austin Passy on twitter">TheFrosty</a></p>';
+		echo '<p id="comment-validation-reloaded-author" style="font-size:80%;"><a class="twitter-anywhere-user" href="http://austinpassy.com/wordpress-plugins/comment-validation-reloaded" title="WordPress Comment Validation Reloaded plugin">Comment validation</a> by @<a href="http://twitter.com/thefrosty" title="Austin Passy on twitter">TheFrosty</a></p>';
 }
 
 /**
